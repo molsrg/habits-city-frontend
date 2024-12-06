dc0d6fe9393d4c81289dd8cff5ec7096ee493151
timealiver <timealive@mail.ru>
2024-12-06 15:57:13 +0300

    fix: fix of fix of fix

--- services/api.service.ts
+++ services/api.service.ts
@@ -103,8 +103,8 @@ class ApiService {
   }
 }
 
-const authService = new ApiService('http://91.108.246.191/auth-service');
-const userService = new ApiService('http://91.108.246.191/user-service');
-const friendService = new ApiService('http://91.108.246.191/user-service');
+const authService = new ApiService('https://91.108.246.191/auth-service');
+const userService = new ApiService('https://91.108.246.191/user-service');
+const friendService = new ApiService('https://91.108.246.191/user-service');
 
 export { authService, friendService, userService };
