1488dbfb1a592233ea4eb17bc4d5b5d48d8a20c9
Molokov Sergey <molsrg.35@gmail.com>
2025-01-14 13:23:58 +0300

    Update locale.global.ts

--- middleware/locale.global.ts
+++ middleware/locale.global.ts
@@ -1,10 +1,14 @@
 // middleware/locale.global.ts
 import { useLocaleStore } from '@/store/locale.store';
 
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to, from) => {
   const localeStore = useLocaleStore();
   if (!localeStore.locale) {
     const savedLocale = localStorage.getItem('language') || 'en';
     localeStore.setLocale(savedLocale);
   }
+
+  // Log the `to` and `from` routes
+  console.log('Navigating from:', from.fullPath);
+  console.log('Navigating to:', to.fullPath);
 });
