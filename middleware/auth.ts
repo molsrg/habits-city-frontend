e51f1d1bb9d6679c685094091cdc723efdab93cf
Molokov Sergey <molsrg.35@gmail.com>
2025-01-14 17:24:30 +0300

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,10 +2,9 @@ import { useTokenStore } from '@/store/token.store';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const tokenStore = useTokenStore();
-
+  console.log(tokenStore.getToken);
+  console.log(tokenStore.getStatus);
   if (!tokenStore.getStatus) {
-    console.log(tokenStore.getToken);
-    console.log(tokenStore.getStatus);
     return navigateTo('/auth/login');
   }
 });
