239dc41d4c09774b02c5f963c701e6f7f25f20ec
Molokov Sergey <molsrg.35@gmail.com>
2025-01-18 13:14:54 +0300

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,13 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const token = useCookie('access_token');
-  if (!token.value) {
-    return navigateTo('/auth/login');
+import { useTokenStore } from '@store/token.store';
+import { useUserStore } from '@store/user.store';
+
+export default defineNuxtRouteMiddleware(async (to) => {
+  const tokenStore = useTokenStore();
+  const userStore = useUserStore();
+  if (import.meta.server) return;
+  if (!tokenStore.getStatus) return navigateTo('/auth/login');
+
+  if (to.path === '/profile') {
+    await userStore.fetchUserInfo();
   }
 });
